1bf07443051e0ae1dba06c2e26eb03ff900546f3
AhmadBot <walkerdashner@gmail.com>
2021-03-21 08:17:36 +0000

    needs shift to be fixed, isn't tested for multiple 3+ of steps

--- main.py
+++ main.py
@@ -18,33 +18,45 @@ output = []
 
 #function for finding 2 number combonations for int
 def split(start,tiles,shift):
-  output = []
+  out = []
   for i in range(start,round(tiles/2)):
-    if(shift>0):
-      output.append([shift,i,tiles-i])
-    else:
-      output.append([i,tiles-i])
-  return output
+    out.append([i,tiles-i])
+    if(shift!=[]):
+      for j in out:
+        out.insert(0,shift)
+  return out
 
+
+#function for returning popped list
+def returnpopped(myList):
+  myList.pop()
+  return myList
 
   
 
 
-output = (split(1,input,-1))
+output = (split(1,input,[]))
+splitPrevAppend = []
 print(output)
 
 
-#loop through every # of possible first nums in step
-for i in range(len(output)):
-  splitPrev = split(i+2,output[i][1]-i,i+1)
-  print(splitPrev)
+#loop through every # of possible steps
+for j in range(2):
+  #loop through every # of possible first nums in step
+  for i in range(len(output)):
+    #split last element of output 
+    #make sure to set start limit to 2nd last element of output
+    splitPrev = split(output[i][len(output[i])-2]+1,output[i][len(output[i])-1],[])
 
+    print(splitPrev)
+    splitPrevAppend = splitPrevAppend + splitPrev
 
-# print(output)
-# print('Length of Output is {}'.format(len(output)))
+  print("----------------------")
+  output=splitPrevAppend
 
 
 
 # timer  
 time_elapsed = datetime.now() - start_time
 print('Time elapsed is {}'.format(time_elapsed))
+
